1927dd22bd6266c680f4fb90714b9961417e6330
Michael Morikawa <mfm29578@gmail.com>
2020-03-06 17:48:21 -0800

    finsihed lab 2

--- Labs/Lab_2_Hash_code_collisions/src/main.cpp
+++ Labs/Lab_2_Hash_code_collisions/src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <time.h>
 #include <vector>
 
 void allLowerCase(std::string& word)
@@ -13,44 +14,29 @@ void allLowerCase(std::string& word)
     }
 }
 
-int hashCode(std::string word)
+int hashCode(std::string word, int shift)
 {
     unsigned int x = 0;
     for (unsigned int i = 0; i < word.size(); i++)
     {
-        x = (x << 13) | (x >> 19);
+        x = (x << shift) | (x >> 32 - shift);
         x += (unsigned int)word[i];
     }
     return (int)x;
 }
 
-int main()
+void collisionTest(std::vector<std::string> wordList, int shift)
 {
-    std::ifstream infile;
-    infile.open("usdeclarPC.txt");
-    std::string word;
-    std::vector<std::string> words;
-    while (infile >> word)
-    {
-        //Removes anything that is not a number or an letter
-        word.erase(std::remove_if(word.begin(), word.end(), [](unsigned char x) { return !std::isalnum(x); }), word.end());
-        allLowerCase(word);
-        words.push_back(word);
-    }
-    /*
-    TODO: Could probably move this into the file processing loop
-    TODO: Skiplist simulation
-    */
     std::map<std::string, int> encounteredWords;
     std::map<int, std::string> collisionTest;
     int code = 0;
     int totalCollisions = 0;
-    for (std::string w : words)
-    {
-        if (encounteredWords.find(w) == encounteredWords.end())
+    std::vector<std::string> collidedWords;
+    for (std::string word : wordList)
+        if (encounteredWords.find(word) == encounteredWords.end())
         {
-            encounteredWords.insert(std::pair<std::string, int>(w, w.size()));
-            code = hashCode(w);
+            encounteredWords.insert(std::pair<std::string, int>(word, word.size()));
+            code = hashCode(word, shift);
             if (collisionTest.find(code) == collisionTest.end())
             {
                 collisionTest.insert(std::pair<int, std::string>(code, word));
@@ -58,8 +44,81 @@ int main()
             else
             {
                 totalCollisions++;
+                collidedWords.push_back(collisionTest.find(code)->second);
+                collidedWords.push_back(word);
             }
         }
+    std::cout << "Cyclic Shift of " << shift << " bits\n"
+              << "Collisions: " << totalCollisions << '\n';
+    for (unsigned int i = 0; i < collidedWords.size(); i += 2)
+    {
+        std::cout << collidedWords[i] << ' ' << collidedWords[i + 1] << '\n';
     }
-    std::cout << words.size() << ' ' << totalCollisions << std::endl;
+}
+
+int main()
+{
+    std::ifstream infile;
+    infile.open("usdeclarPC.txt");
+    std::vector<std::string> wordList;
+    std::string word;
+    //collision testing
+    while (infile >> word)
+    {
+        //Removes anything that is not a number or an letter
+        word.erase(std::remove_if(word.begin(), word.end(), ispunct), word.end());
+        allLowerCase(word);
+        wordList.push_back(word);
+    }
+    infile.close();
+    collisionTest(wordList, 1);
+    std::cout << '\n';
+    collisionTest(wordList, 5);
+    std::cout << '\n';
+    collisionTest(wordList, 13);
+    std::cout << '\n';
+    //Skiplist simulation
+
+    int elements = 100;
+    int attempts = 10;
+    int sumLevel = 0;
+    int levels[100] = {0};
+    int currentLevel = 0;
+    int topLevel = 0;
+    int maxLevel = 0;
+    std::srand(time(NULL));
+    for (int i = 0; i < attempts; i++)
+    {
+        for (int i = 0; i < elements; i++)
+        {
+            levels[0]++;
+            while (rand() % 2)
+            {
+                currentLevel++;
+                levels[currentLevel]++;
+            }
+            topLevel = std::max(topLevel, currentLevel);
+            currentLevel = 0;
+        }
+        sumLevel += topLevel;
+        maxLevel = std::max(topLevel, maxLevel);
+        topLevel = 0;
+    }
+    std::cout << "\nSkipList simulation:\n"
+              << "Attempts: " << attempts << "    Elements: " << elements << "\n"
+              << "Max levels: " << maxLevel
+              << "\nAverage levels: " << (float)sumLevel / attempts
+              << "\nAverage elements per level:\n";
+
+    for (int i = 0; i < 100; i++)
+    {
+        if (levels[i] == 0)
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Level " << i << ": " << (float)levels[i] / attempts << '\n';
+        }
+    }
 }
